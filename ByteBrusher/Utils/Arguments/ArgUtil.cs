b2e79c28322f88fc328d9479c524b30e5852cabf
NicoFilips <35654361+NicoSoftware@users.noreply.github.com>
2023-10-06 18:00:38 +0200

    Init Commit

---
+++ ByteBrusher/Utils/Arguments/ArgUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBrusher.Utils.Arguments
+{
+    public static class ArgUtil
+    {
+        public static void ShowHelp()
+        {
+            Console.WriteLine("Help:");
+            Console.WriteLine("Deine Hilfeinformationen hier...");
+        }
+    }
+}
---
+++ ByteBrusher/Utils/ScanFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBrusher.Utils
+{
+    public static class ScanFile
+    {
+        static bool isDuplicate(string directoryPath)
+        {
+            var files = Directory.GetFiles(directoryPath);
+            var fileHashes = new Dictionary<string, List<string>>();
+
+            foreach (var file in files)
+            {
+                string fileHash = ComputeSha256Hash(file);
+                if (fileHashes.ContainsKey(fileHash))
+                {
+                    fileHashes[fileHash].Add(file);
+                }
+                else
+                {
+                    fileHashes[fileHash] = new List<string> { file };
+                }
+            }
+            return true;
+        }
+
+        static string ComputeSha256Hash(string filename)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                using (FileStream stream = File.OpenRead(filename))
+                {
+                    byte[] hash = sha256.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+    }
+}
