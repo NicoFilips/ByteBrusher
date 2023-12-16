aa7bea5018774c772673c75fa574f3c09b2131a9
NicoFilips <git@nicofilips.de>
2023-12-16 19:04:11 +0100

    feat: add TempDirectory

---
+++ Tests/ByteBrusher.Tests.Util/TemporaryDirectory.cs
@@ -0,0 +1,21 @@
+﻿namespace ByteBrusher.Tests.Util;
+
+public sealed record TemporaryDirectory : IDisposable
+{
+    public string DirectoryPath { get; }
+
+    private TemporaryDirectory(string prefix = "_bytebrusher_unit_test")
+    {
+        DirectoryPath = Path.Combine(Path.GetTempPath(), CreateRandDirectoryName(prefix));
+        Directory.CreateDirectory(DirectoryPath);
+    }
+
+    public static TemporaryDirectory Create(string prefix = "_bytebrusher_unit_test") => new(prefix);
+    public static string CreateRandDirectoryName(string prefix) => string.Join("_", prefix, Path.GetRandomFileName());
+
+    public void Dispose()
+    {
+        if (Directory.Exists(DirectoryPath))
+            Directory.Delete(DirectoryPath, true);
+    }
+}
