db93b588393d36840e5f0a191ce5e100926d5bb3
Nico Filips <nico.filips@de.sii.group>
2024-01-17 14:40:30 +0100

    chore: edit comment

--- ByteBrusher.Util.Abstraction/Delete/DeleteUtil.cs
+++ ByteBrusher.Util.Abstraction/Delete/DeleteUtil.cs
@@ -6,7 +6,7 @@ namespace ByteBrusher.Util.Abstraction.Delete;
 public interface IDeleteUtil
 {
     /// <summary>
-    /// Deletes all duplicates in the list
+    /// Deletes duplicate entrys in the list
     /// </summary>
     /// <returns>Monad of the result</returns>
     public ErrorOr<Deleted> TryDelete(List<FoundFile> duplicates);
