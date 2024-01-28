e89a5c4d00e389858c7e85641a0ae6c00393d33f
NicoFilips <git@nicofilips.de>
2024-01-28 16:13:07 +0100

    fix(sonarcloud): code smells

--- ByteBrusher/Core/Parameter/ByteBrusherParams.cs
+++ ByteBrusher/Core/Parameter/ByteBrusherParams.cs
@@ -2,4 +2,8 @@ namespace ByteBrusher.Core.Parameter;
 
 public class ByteBrusherParams : IByteBrusherParams
 {
+    public string Path { get; set; } = string.Empty;
+    public bool DeleteFlag { get; set; }
+    public bool IncludeVideos { get; set; }
+    public bool IncludeDocuments { get; set; }
 }
